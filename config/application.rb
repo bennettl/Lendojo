674b980e87900169072f7dc5645ae90c0b17e36e
Bennett Lee <bennettl908@yahoo.com>
2014-06-14 01:58:40 -0700

    Heroku push update

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,7 @@ module LendojoApp
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
 
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.assets.precompile += %w( *.css *.js)
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
