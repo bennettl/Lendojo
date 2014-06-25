a7e5537f11ae928926b6d2ae903567c71d649c4f
Bennett Lee <bennettl908@yahoo.com>
2014-06-25 01:53:21 -0700

    heroku update

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,8 @@ Bundler.require(*Rails.groups)
 
 module LendojoApp
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
     # mail
     config.action_mailer.delivery_method = :smtp
