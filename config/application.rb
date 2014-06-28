2a356e91b4aba717699beb69ed2a09d642bb6609
Bennett Lee <bennettl908@yahoo.com>
2014-06-28 12:51:16 -0700

    Added swagger documentation. Ovewrote devise methods and http authentication for debugging purposes

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module LendojoApp
   class Application < Rails::Application
-    # config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     # mail
     config.action_mailer.delivery_method = :smtp
